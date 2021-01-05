85cfb28ed26fa6f1049d2401559a8790bf21d2c3
Darwin Camahalan <camahalandarwin@gmail.com>
2021-01-05 09:40:37 +0800

    Vesion 1.01

--- lib/Screens/About/main.dart
+++ lib/Screens/About/main.dart
@@ -1,5 +1,9 @@
+import 'package:daily_note/Screens/Blogs/my_blogs.dart';
+import 'package:daily_note/Screens/Business/main.dart';
 import 'package:daily_note/Screens/Home/components/body.dart';
 import 'package:daily_note/Screens/Login/login_screen.dart';
+import 'package:daily_note/Screens/Notes/my_notes.dart';
+import 'package:daily_note/Screens/Webster/webster_screen.dart';
 import 'package:daily_note/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -23,75 +27,184 @@ class AboutUs extends StatelessWidget {
                 child: new ListView(
                   children: [
                     new Container(
-                      child: new DrawerHeader(
-                          child: new CircleAvatar(
-                        radius: 30.0,
-                        child: Image.asset('assets/images/profile.png'),
-                        backgroundColor: Colors.transparent,
-                      )),
-                    ),
-                    Text(
-                      'Admin',
-                      style: GoogleFonts.poppins(
-                          fontWeight: FontWeight.bold, fontSize: 20),
-                      textAlign: TextAlign.center,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 50, left: 10),
-                      child: Column(children: [
-                        GestureDetector(
-                          onTap: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) {
-                                  return AboutUs();
-                                },
+                      child: Center(
+                        child: Column(
+                          children: <Widget>[
+                            new UserAccountsDrawerHeader(
+                              decoration: BoxDecoration(color: Colors.white),
+                              accountName: new Text(
+                                'User',
+                                style: GoogleFonts.poppins(
+                                    fontWeight: FontWeight.bold, fontSize: 20),
                               ),
-                            );
-                          },
-                          child: Row(
-                            children: [
-                              Icon(Icons.info_outline_rounded),
-                              Container(
-                                width: 85,
+                              accountEmail: new Text('example@gmail.com'),
+                              currentAccountPicture: new CircleAvatar(
+                                radius: 50.0,
+                                backgroundColor: const Color(0xFF778899),
+                                backgroundImage: NetworkImage(
+                                    "https://cdn1.iconfinder.com/data/icons/avatars-1-5/136/87-512.png"),
                               ),
-                              Text('About Us',
-                                  style: GoogleFonts.poppins(
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 15)),
-                            ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Column(children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          child: GestureDetector(
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) {
+                                    return MyNotes();
+                                  },
+                                ),
+                              );
+                            },
+                            child: Row(
+                              children: [
+                                Icon(Icons.note),
+                                Container(
+                                  width: 20,
+                                ),
+                                Text('All Notes',
+                                    style: GoogleFonts.poppins(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 15)),
+                              ],
+                            ),
                           ),
                         ),
-                        Container(
-                          height: 340,
-                        ),
-                        GestureDetector(
-                          onTap: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) {
-                                  return LoginScreen();
-                                },
-                              ),
-                            );
-                          },
-                          child: Row(
-                            children: [
-                              Icon(Icons.exit_to_app_sharp),
-                              Container(
-                                width: 95,
-                              ),
-                              Text('Logout',
-                                  style: GoogleFonts.poppins(
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 15)),
-                            ],
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          child: GestureDetector(
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) {
+                                    return MyBlogs();
+                                  },
+                                ),
+                              );
+                            },
+                            child: Row(
+                              children: [
+                                Icon(Icons.notes),
+                                Container(
+                                  width: 20,
+                                ),
+                                Text('All  Blogs',
+                                    style: GoogleFonts.poppins(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 15)),
+                              ],
+                            ),
                           ),
                         ),
-                      ]),
-                    )
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          child: GestureDetector(
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) {
+                                    return Webster();
+                                  },
+                                ),
+                              );
+                            },
+                            child: Row(
+                              children: [
+                                Icon(Icons.book),
+                                Container(
+                                  width: 20,
+                                ),
+                                Text('Mini Webster',
+                                    style: GoogleFonts.poppins(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 15)),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          child: GestureDetector(
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) {
+                                    return Business();
+                                  },
+                                ),
+                              );
+                            },
+                            child: Row(
+                              children: [
+                                Icon(Icons.calendar_today),
+                                Container(
+                                  width: 20,
+                                ),
+                                Text('Business',
+                                    style: GoogleFonts.poppins(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 15)),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        height: 272,
+                      ),
+                      Container(
+                        color: kPrimaryColor,
+                        child: Padding(
+                          padding: const EdgeInsets.only(
+                              top: 15, bottom: 15, left: 5),
+                          child: GestureDetector(
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) {
+                                    return LoginScreen();
+                                  },
+                                ),
+                              );
+                            },
+                            child: Row(
+                              children: [
+                                Icon(
+                                  Icons.exit_to_app_sharp,
+                                  color: Colors.white,
+                                ),
+                                Container(
+                                  width: 20,
+                                ),
+                                Text('Logout',
+                                    style: GoogleFonts.poppins(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 15,
+                                        color: Colors.white)),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ]),
                   ],
                 ))),
         appBar: AppBar(
--- lib/Screens/Notes/my_notes.dart
+++ lib/Screens/Notes/my_notes.dart
@@ -1,6 +1,11 @@
+import 'package:daily_note/Screens/About/main.dart';
+import 'package:daily_note/Screens/Blogs/my_blogs.dart';
+import 'package:daily_note/Screens/Business/main.dart';
 import 'package:daily_note/Screens/Home/components/body.dart';
+import 'package:daily_note/Screens/Login/login_screen.dart';
 import 'package:daily_note/Screens/Notes/add_notes.dart';
 import 'package:daily_note/Screens/Notes/note_provider.dart';
+import 'package:daily_note/Screens/Webster/webster_screen.dart';
 import 'package:daily_note/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -26,7 +31,190 @@ class _MyNotesState extends State<MyNotes> {
         backgroundColor: kPrimaryLightColor,
         drawer: new Drawer(
             child: Container(
-                color: Colors.orangeAccent[100], child: new ListView())),
+                color: kPrimaryLightColor,
+                child: new ListView(
+                  children: [
+                    new Container(
+                      child: Center(
+                        child: Column(
+                          children: <Widget>[
+                            new UserAccountsDrawerHeader(
+                              decoration: BoxDecoration(color: Colors.white),
+                              accountName: new Text(
+                                'User',
+                                style: GoogleFonts.poppins(
+                                    fontWeight: FontWeight.bold, fontSize: 20),
+                              ),
+                              accountEmail: new Text('example@gmail.com'),
+                              currentAccountPicture: new CircleAvatar(
+                                radius: 50.0,
+                                backgroundColor: const Color(0xFF778899),
+                                backgroundImage: NetworkImage(
+                                    "https://cdn1.iconfinder.com/data/icons/avatars-1-5/136/87-512.png"),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Column(children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          child: GestureDetector(
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) {
+                                    return MyBlogs();
+                                  },
+                                ),
+                              );
+                            },
+                            child: Row(
+                              children: [
+                                Icon(Icons.notes),
+                                Container(
+                                  width: 20,
+                                ),
+                                Text('All  Blogs',
+                                    style: GoogleFonts.poppins(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 15)),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          child: GestureDetector(
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) {
+                                    return Webster();
+                                  },
+                                ),
+                              );
+                            },
+                            child: Row(
+                              children: [
+                                Icon(Icons.book),
+                                Container(
+                                  width: 20,
+                                ),
+                                Text('Mini Webster',
+                                    style: GoogleFonts.poppins(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 15)),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          child: GestureDetector(
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) {
+                                    return Business();
+                                  },
+                                ),
+                              );
+                            },
+                            child: Row(
+                              children: [
+                                Icon(Icons.calendar_today),
+                                Container(
+                                  width: 20,
+                                ),
+                                Text('Business',
+                                    style: GoogleFonts.poppins(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 15)),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          child: GestureDetector(
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) {
+                                    return AboutUs();
+                                  },
+                                ),
+                              );
+                            },
+                            child: Row(
+                              children: [
+                                Icon(Icons.info_outline_rounded),
+                                Container(
+                                  width: 20,
+                                ),
+                                Text('About Us',
+                                    style: GoogleFonts.poppins(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 15)),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        height: 272,
+                      ),
+                      Container(
+                        color: kPrimaryColor,
+                        child: Padding(
+                          padding: const EdgeInsets.only(
+                              top: 15, bottom: 15, left: 5),
+                          child: GestureDetector(
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) {
+                                    return LoginScreen();
+                                  },
+                                ),
+                              );
+                            },
+                            child: Row(
+                              children: [
+                                Icon(
+                                  Icons.exit_to_app_sharp,
+                                  color: Colors.white,
+                                ),
+                                Container(
+                                  width: 20,
+                                ),
+                                Text('Logout',
+                                    style: GoogleFonts.poppins(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 15,
+                                        color: Colors.white)),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ]),
+                  ],
+                ))),
         appBar: AppBar(
           centerTitle: true,
           iconTheme: IconThemeData(color: Colors.white),
